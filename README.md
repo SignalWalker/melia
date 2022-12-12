8984ecbdf92b861c34bda10e5e398eebdb280cb5
Ash Walker <ashurstwalker@gmail.com>
2022-12-12 12:38:55 -0500

    build: flake.nix

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 <p align="center">
   <a href="https://github.com/SignalWalker/melia/commits/main"><img src="https://img.shields.io/github/commits-since/SignalWalker/melia/0.1.0" alt="commits since last release"/></a>
-  <a href="https://opensource.org/licenses/agpl-license"><img src="https://img.shields.io/github/license/SignalWalker/melia" alt="AGPL 3.0 or later"/></a>
+  <a href="https://opensource.org/licenses/AGPL-3.0"><img src="https://img.shields.io/github/license/SignalWalker/melia" alt="AGPL 3.0"/></a>
 </p>
 
-A web server designed for [https://ashwalker.net/].
+A web server designed for [ashwalker.net](https://ashwalker.net).
 
 ## See Also
 
--- flake.nix
+++ flake.nix
@@ -6,15 +6,41 @@
       url = "github:kamadorueda/alejandra";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs @ {
     self,
     nixpkgs,
+    crane,
     ...
   }:
     with builtins; let
       std = nixpkgs.lib;
+      systems = attrNames crane.lib; # systems supported by crane
     in {
       formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
+      packages = std.genAttrs systems (system: let
+        crane = inputs.crane.lib.${system};
+      in {
+        default = self.packages.${system}.melia;
+        melia = crane.buildPackage {
+          src = crane.cleanCargoSource ./.;
+        };
+      });
+      overlays.default = final: prev: {
+        melia = self.packages.${final.crossSystem}.melia;
+      };
+      apps =
+        std.mapAttrs (system: pkgs: {
+          melia = {
+            type = "app";
+            program = "${pkgs.melia}/bin/melia";
+          };
+          default = self.apps.${system}.melia;
+        })
+        self.packages;
     };
 }
